89854140e2a80037341bf573a50fefe3b8d05bbc
Totilla <tsm.antonia@gmail.com>
2024-07-31 20:25:24 +0200

    componente header creado

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,23 @@
+let w = window.innerWidth;
+
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+   w = window.innerWidth;
+   ancho = "100%";
+
+   if(w > 500){
+    ancho = "50%";
+   }
+    document.getElementById("myNav").style.width = ancho;
   }
   
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+}); 
